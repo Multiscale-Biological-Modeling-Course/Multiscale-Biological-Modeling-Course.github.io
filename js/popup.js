6b3b43e35916f718a0c70d258f9f2d60cff6e75b
Phillip Compeau <phcompeau@users.noreply.github.com>
2021-11-10 11:53:57 -0500

    trying popup

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
